12a74c59912cb1e88e13a59062f649403155fe04
Travis Nguyen <travisnguyenhoang@gmail.com>
2022-01-26 21:48:17 +0700

    Add some keymap

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,6 +94,9 @@ return packer.startup(function(use)
   -- Git
   use "lewis6991/gitsigns.nvim"
 
+  -- Markdown
+  use "ellisonleao/glow.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
