cb2d0dad9d5d44972befa977f54f0da6f58fae85
Travis Nguyen <travis@lingble.com>
2022-07-25 18:02:19 +0700

    Remove some useless plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -51,13 +51,13 @@ return packer.startup(function(use)
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
   use "nvim-lualine/lualine.nvim"
---   use "akinsho/toggleterm.nvim"
-  use "ahmedkhalf/project.nvim"
+  --  use "akinsho/toggleterm.nvim"
+  --  use "ahmedkhalf/project.nvim"
   use "lewis6991/impatient.nvim"
   use "lukas-reineke/indent-blankline.nvim"
---  use "goolord/alpha-nvim"
+  --  use "goolord/alpha-nvim"
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
---  use "folke/which-key.nvim"
+  --  use "folke/which-key.nvim"
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
@@ -72,8 +72,8 @@ return packer.startup(function(use)
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
 
   -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  -- use "L3MON4D3/LuaSnip" --snippet engine
+  -- use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
@@ -82,7 +82,7 @@ return packer.startup(function(use)
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
   -- Telescope
---  use "nvim-telescope/telescope.nvim"
+  --  use "nvim-telescope/telescope.nvim"
 
   -- Treesitter
   use {
@@ -137,7 +137,8 @@ use "windwp/nvim-spectre"
 
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
-if PACKER_BOOTSTRAP then
-  require("packer").sync()
-end
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+
 end)
