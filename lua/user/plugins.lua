ae4774283b7c10de8743d558ef5528f6b923703e
Travis Nguyen <travisnguyenhoang@gmail.com>
2022-07-28 17:12:44 +0700

    fix cmp small issue

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -72,8 +72,8 @@ return packer.startup(function(use)
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
 
   -- snippets
-  -- use "L3MON4D3/LuaSnip" --snippet engine
-  -- use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
