64c8abdab61a6bcfabbaa3fa138ef3d5b847d474
Travis Nguyen <travisnguyenhoang@gmail.com>
2022-11-14 22:36:15 +0700

    Add schemastore style for .json .lock file

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -165,6 +165,20 @@ use { "anuvyklack/windows.nvim",
       require('windows').setup()
    end
 }
+-- Lua
+--[[ use { ]]
+--[[   "folke/trouble.nvim", ]]
+--[[   requires = "kyazdani42/nvim-web-devicons", ]]
+--[[   config = function() ]]
+--[[     require("trouble").setup { ]]
+--[[       -- your configuration comes here ]]
+--[[       -- or leave it empty to use the default settings ]]
+--[[       -- refer to the configuration section below ]]
+--[[     } ]]
+--[[   end ]]
+--[[ } ]]
+
+use "b0o/schemastore.nvim"
 
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
