b8593bad5df7ff04a77ad3875d3b6d6fab5f3366
Travis Nguyen <travisnguyenhoang@gmail.com>
2023-04-20 16:37:44 +0700

    Update nvim-tree config

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -13,12 +13,6 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
@@ -49,12 +43,7 @@ nvim_tree.setup {
     hide_root_folder = false,
     side = "left",
     mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
+      custom_only = false
     },
     number = false,
     relativenumber = false,
