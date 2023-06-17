bd28d9e9e6899a5d786eb986bbcf5d3df0d192c6
Travis Nguyen <travisnguyenhoang@gmail.com>
2023-06-17 10:35:22 +0700

    Fix nvimtree upgrade redundancy config

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -9,16 +9,14 @@ if not config_status_ok then
 end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
+local api = require('nvim-tree.api')
+
+api.config.mappings.default_on_attach(bufnr)
+
 
 nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
@@ -48,14 +46,6 @@ nvim_tree.setup {
     width = 45,
     hide_root_folder = false,
     side = "left",
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
     number = false,
     relativenumber = false,
   },
